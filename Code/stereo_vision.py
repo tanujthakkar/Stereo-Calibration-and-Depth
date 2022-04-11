924e648fa5c10c481d56109ee844c6f6a22379c5
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-04-11 18:49:54 -0400

    Added parser, started StereoVision, added parse_params

--- Code/stereo_vision.py
+++ Code/stereo_vision.py
@@ -21,3 +21,24 @@ import argparse
 import time
 
 from utils import *
+
+
+class StereoVision:
+
+	def __init__(self, data_dir: str) -> None:
+		self.data_dir = get_data_files(data_dir)
+		self.calib_params = parse_params(self.data_dir[0])
+
+def main():
+	Parser = argparse.ArgumentParser()
+	Parser.add_argument('--DataDir', type=str, default="../Data/curule/", help='Path to the input data directory')
+	Parser.add_argument('--Visualize', action='store_true', help='Toggle visualization')
+
+	Args = Parser.parse_args()
+	data_dir = Args.DataDir
+	visualize = Args.Visualize
+
+	SV = StereoVision(data_dir)
+
+if __name__ == '__main__':
+	main()
